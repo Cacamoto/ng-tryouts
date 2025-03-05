ecb7b753ed12e0b4b14e9fab5ccd018920d6d5a3
Benas <mockusben@gmail.com>
2025-03-06 00:13:41 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, LoginScreenComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <header>
+      <mat-icon
+        aria-hidden="true"
+        aria-label="Stores Icon"
+        fontIcon="warehouse"
+      />
+      <h1>{{ title }}</h1>
+    </header>
+    <main>
+      @if (2 > 1) {
+      <app-login-screen />
+      }
+    </main>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [
+    'header {background-color: rgba(0, 0, 0, 0.1); display: flex; justify-content: center; align-items: center; padding: 0; margin: 0; gap: 4px;}',
+    'h1 {text-transform: uppercase;}',
+    'mat-icon {color: rgba(0, 0, 0, 0.8); font-size: 2rem; width: 2rem; height: 2rem;}',
+    'main { display: flex; justify-content: center; align-items: start; margin-top: 1rem} ',
+  ],
 })
 export class AppComponent {
-  title = 'ng-tryouts';
+  title = 'Prekių atsiėmimo terminalas';
 }
---
+++ src/app/login-screen/login-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-screen',
+  imports: [],
+  template: `
+    <p>
+      login-screen works!
+    </p>
+  `,
+  styles: ``
+})
+export class LoginScreenComponent {
+
+}
